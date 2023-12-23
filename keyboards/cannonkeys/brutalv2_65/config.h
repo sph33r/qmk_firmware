d45778e13dada190f8508500c9c98b070389a93f
Brad Haase <b@bwh.me>
2023-12-23 09:00:08 -0500

    Adding 1up and cannon key map attempts

--- keyboards/cannonkeys/brutalv2_65/config.h
+++ keyboards/cannonkeys/brutalv2_65/config.h
@@ -18,9 +18,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+//#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+//#define LOCKING_RESYNC_ENABLE
+
+/* Enable capsword on left shift */
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 
 /*
  * Feature disable options
--- keyboards/cannonkeys/brutalv2_65/rules.mk
+++ keyboards/cannonkeys/brutalv2_65/rules.mk
@@ -5,12 +5,13 @@ DFU_SUFFIX_ARGS = -v FFFF -p FFFF
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+CAPS_WORD_ENABLE = yes      # Allow capsword macros
 
