87f3283c15232c75284b9377269c8eac8d15f236
Brad Haase <b@bwh.me>
2024-02-05 16:47:36 -0500

    Moving rules and config in with the brutalv2_65 keymaps

--- keyboards/cannonkeys/brutalv2_65/keymaps/sph33r/rules.mk
+++ keyboards/cannonkeys/brutalv2_65/keymaps/sph33r/rules.mk
@@ -13,4 +13,5 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+CAPS_WORD_ENABLE = yes      # Allow capsword macros
 
