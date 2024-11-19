330c9029b5ddc3588f634c5334e908fab5aff3c9
Brad Haase <b@bwh.me>
2024-11-19 14:37:35 -0500

    Switching back to KC_MINS as numpad mins doesn't work.

--- keyboards/crkbd/keymaps/sph33r_no_rgb_hrm/keymap.c
+++ keyboards/crkbd/keymaps/sph33r_no_rgb_hrm/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TILD, KC_EXLM,  KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_NO,   KC_NO,  KC_NO,   KC_UNDS, KC_PMNS,                       KC_PIPE, KC_LBRC, KC_RBRC,  KC_NO,  KC_NO,  KC_NO,
+      _______, KC_NO,   KC_NO,  KC_NO,   KC_UNDS, KC_MINS,                       KC_PIPE, KC_LBRC, KC_RBRC,  KC_NO,  KC_NO,  KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_NO,  KC_BTN1, KC_BTN2, KC_EQL,  KC_PLUS,                       KC_BSLS, KC_LCBR, KC_RCBR,  KC_NO,  KC_NO,  _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
