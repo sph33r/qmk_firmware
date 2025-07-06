9f6104dd40274247fec2a78c66bb32b353a94499
Brad Haase <b@bwh.me>
2025-07-06 18:21:10 -0400

    Tweaking the Planck map and adding a visual readme

--- keyboards/planck/keymaps/sph33r/keymap.c
+++ keyboards/planck/keymaps/sph33r/keymap.c
@@ -23,12 +23,6 @@ enum planck_layers {
   _ADJUST
 };
 
-enum planck_keycodes {
-  QWERTY = SAFE_RANGE,
-  BACKLIT,
-  EXT_PLV
-};
-
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
@@ -38,16 +32,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-    KC_LSFT, KC_LGUI, KC_LALT, KC_LBRC, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RBRC, KC_LEFT, KC_RIGHT, KC_ENT
+    SC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  SC_RSPC,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_PGDN, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_PGUP, KC_LEFT, KC_RIGHT, KC_ENT
 ),
 
 /* Lower */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_DEL,
-    _______, _______, KC_PSCR, _______, _______, _______, KC_PIPE, _______, _______, _______, _______, _______,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+    _______, _______, _______, _______, KC_UNDS, KC_MINS, KC_PIPE, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, KC_EQL,  KC_PLUS, KC_BSLS, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Raise */
@@ -55,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_RGHT, KC_DOWN, KC_UP,   KC_LEFT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise) */
@@ -68,3 +62,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
--- keyboards/planck/keymaps/sph33r/rules.mk
+++ keyboards/planck/keymaps/sph33r/rules.mk
@@ -2,3 +2,4 @@ ifeq ($(strip $(AUDIO_ENABLE)), yes)
     SRC += muse.c
 endif
 CAPS_WORD_ENABLE = yes
+COMMAND_ENABLE = no
