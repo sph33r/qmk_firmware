51d009034a833e3a85dfc9a72acfea7c6a8f2237
Brad Haase <b@bwh.me>
2026-04-30 14:17:18 -0400

    Adding layer tap and fixing LDUR

--- keyboards/planck/keymaps/sph33r/keymap.c
+++ keyboards/planck/keymaps/sph33r/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty */
 [_QWERTY] = LAYOUT_planck_grid(
     QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+    LT(_RAISE, KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
     SC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  SC_RSPC,
     KC_LCTL, KC_LGUI, KC_LALT, KC_PGDN, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_PGUP, KC_LEFT, KC_RIGHT, KC_ENT
 ),
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Raise */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_RGHT, KC_DOWN, KC_UP,   KC_LEFT, _______, _______,
+    _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
     _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ ,_______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT
+    RM_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT
 )
 
 };
